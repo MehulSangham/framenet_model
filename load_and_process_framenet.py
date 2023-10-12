8849a79875ae33c4ed6579f346443f33b23f4103
MehulSangham <mehulsangham@gmail.com>
2023-10-12 08:56:56 -0700

    updating load and process framenet data

--- load_and_process_framenet.py
+++ load_and_process_framenet.py
@@ -3,20 +3,35 @@ import os
 import pandas as pd
 from nltk.corpus import framenet as fn
 import pickle
+import nltk
 
+# Function to convert FrameNet frame to dictionary
 def frame_to_dict(frame):
     return {'id': frame.ID, 'name': frame.name, 'definition': frame.definition}
 
+# Specify the path to FrameNet data
 fn_data_path = '/users/mehulsangham/nltk_data/corpora/framenet_v17'
+# Specify the path to save processed data
 processed_data_path = '/Users/mehulsangham/GH Projects/framenet_model/processed_framenet_data.pkl'
 
+# Check if FrameNet data exists
 if os.path.exists(fn_data_path):
     print('FrameNet data is found.')
-    fn_frames = fn.frames()
-    frames_list = [frame_to_dict(frame) for frame in fn_frames]
-    df = pd.DataFrame(frames_list)
-    with open(processed_data_path, 'wb') as f:
-        pickle.dump(df, f)
-    print('FrameNet data is processed and saved.')
 else:
-    print('FrameNet data is not found.')
+    print('FrameNet data is not found. Downloading...')
+    nltk.download('framenet_v17')
+    print('FrameNet data downloaded.')
+
+# Load FrameNet frames
+fn_frames = fn.frames()
+
+# Convert each frame to a dictionary and store in a list
+frames_list = [frame_to_dict(frame) for frame in fn_frames]
+
+# Convert the list of dictionaries to a DataFrame
+df = pd.DataFrame(frames_list)
+
+# Save the DataFrame to a pickle file
+with open(processed_data_path, 'wb') as f:
+    pickle.dump(df, f)
+print('FrameNet data is processed and saved.')
